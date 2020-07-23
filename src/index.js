15947064043a0eae1afd04b9b3badaabf9302280
Kyle Luke <luke.kylew@gmail.com>
2020-07-22 21:57:43 -0600

    Set up React Bootstrap

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import 'foundation-sites/dist/css/foundation.min.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import manageProjects from './reducers/manageProjects'
 import { Provider } from 'react-redux';
 import { createStore, compose, applyMiddleware } from 'redux';
