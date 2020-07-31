350659a0a2153f25c5b49c66da9543e74eae5be1
Kyle Luke <luke.kylew@gmail.com>
2020-07-30 21:45:02 -0600

    Added About component in Nav dropdown and styled.

--- src/components/about.js
+++ src/components/about.js
@@ -1,18 +1,45 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
-import { Container, Row, Col } from "react-bootstrap"
+import { Row, Col, Button } from "react-bootstrap"
+import { AiFillLinkedin, AiFillFacebook } from "react-icons/ai"
+import { FaMountain, FaDev, FaGithubSquare } from "react-icons/fa"
+import { MdLaptopMac, MdPhotoLibrary, MdEmail } from "react-icons/md"
 // import Logo from '../images/KLogo.svg'
+
+
+
 
 const About = ({ siteTitle }) => (
     <div
+        className="about"
         style={{
           margin: `2vh auto`,
           maxWidth: '90vw',
           padding: `0 auto`,
         }}
       >
-    About Me
+      <h2>Kyle Luke</h2>
+        <Row className="about-icons">
+            <Col><MdLaptopMac /><br />Develop</Col>
+            <Col><FaMountain /><br />Adventure</Col>
+            <Col><MdPhotoLibrary /><br />Design</Col>
+        </Row>
+        <Col>
+            <p><i>"But I don't want comfort. I want poetry. I want danger. I want freedom. I want goodness. I want sin."</i><br />&mdash; Brave New World</p>
+            <p>Creating things is my jam.<br />Visually engaging design.<br />Purposeful and story driven content.<br />Continuously learning.<br />Pushing boundaries.</p>
+        </Col>
+        <Row className="contact-icons">
+            <Col><Button variant="dark" href="/" >Download Resume</Button></Col>
+        </Row>
+        <hr />
+        <Row className="contact-icons">
+            <Col><Button variant="dark" href="https://www.linkedin.com/in/kylewluke/" target=""><AiFillLinkedin /></Button></Col>
+            <Col><Button variant="dark" href="https://github.com/lukekyl" target="_blank"><FaGithubSquare /></Button></Col>
+            <Col><Button variant="dark" href="https://dev.to/lukekyl" target="_blank"><FaDev /></Button></Col>
+            <Col><Button variant="dark" href="mailto:luke.kylew@gmail.com" ><MdEmail /></Button></Col>
+        </Row>
+        <Link to='/'><small>kyleluke.dev</small></Link>
     </div>
 )
 
