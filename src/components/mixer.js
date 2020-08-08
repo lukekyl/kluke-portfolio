2281dc5951584c12325f1111d5649a4a5f5c7fee
Kyle Luke <luke.kylew@gmail.com>
2020-08-08 14:04:21 -0600

    MixItUp fix for server side build.

--- src/components/mixer.js
+++ src/components/mixer.js
@@ -1,11 +1,16 @@
 import React from "react"
-import mixitup from "mixitup"
+
+
 
 const Mixer = () => {
     // MixItUp Settings
-    
-    if (typeof window !== "undefined") {
+
     const containerEl = document.querySelector(".project-grid")
+
+    if (typeof window !== "undefined") {
+
+        const mixitup = require("mixitup") 
+    
 
         if (containerEl) {
             const mixer = mixitup(containerEl, {
@@ -23,13 +28,12 @@ const Mixer = () => {
 
             mixer.filter(".dev, .frontend, .backend, .js, .react, .rails, .design")
 
-            
+
         } else {
             console.log('No containerEl!')
         }
 
     }
-
     return <></>
 
 }
