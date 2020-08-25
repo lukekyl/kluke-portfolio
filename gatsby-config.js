ebb2c84296b2bd74e5a4f695198b240328752bbc
Kyle Luke <luke.kylew@gmail.com>
2020-08-25 11:01:06 -0600

    Update to styling of nav menu on focus, added google analytics.

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,6 +28,13 @@ module.exports = {
         icon: `src/images/KLogo.svg`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // replace "UA-XXXXXXXXX-X" with your own Tracking ID
+        trackingId: "UA-101112394-2",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
